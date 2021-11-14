4d291f9a266962701569fd5e551cb7f159e5dac2
Esteban430 <itemp1240@gmail.com>
2021-11-13 22:05:24 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9c43801b-64ac-4be4-a7c3-f0d558544521';
+export const MODAL_CLOSE_SEC = 2.5;
